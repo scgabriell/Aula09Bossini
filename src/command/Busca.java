5693ccf8119336681cf2df7ad16256ca87c7b73d
scgabriell <62763530+scgabriell@users.noreply.github.com>
2020-05-12 18:52:59 -0300

    Create Busca.java

---
+++ src/command/Busca.java
@@ -0,0 +1,20 @@
+  
+package command;
+
+import java.util.ArrayList;
+
+import model.Pais;
+
+public class Busca {
+	public static int busca(Pais pais, ArrayList<Pais> lista) {
+		System.out.println("PAIS pra alterar: " + pais);
+		Pais to;
+		for (int i = 0; i < lista.size(); i++) {
+			to = lista.get(i);
+			if(to.getId() == pais.getId()) {
+				return i;
+			}
+		}
+		return -1;
+	}
+}
